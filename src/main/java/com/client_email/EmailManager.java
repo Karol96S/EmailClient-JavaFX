987be6f214b4220508fd8e87f47d27e2b12fd9f6
Karol_S <szypa288@gmail.com>
2023-03-16 06:13:04 +0100

    email messages now show dinamically

--- src/main/java/com/client_email/EmailManager.java
+++ src/main/java/com/client_email/EmailManager.java
@@ -1,22 +1,38 @@
 package com.client_email;
 
 import com.client_email.controller.services.FetchFoldersService;
+import com.client_email.controller.services.FolderUpdaterService;
 import com.client_email.model.EmailAccount;
 import com.client_email.model.EmailTreeItem;
 import javafx.scene.control.TreeItem;
 
+import javax.mail.Folder;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmailManager {
 
     //Folders handling:
+    private FolderUpdaterService folderUpdaterService;
     private EmailTreeItem<String> foldersRoot = new EmailTreeItem<String>("");
 
     public EmailTreeItem<String> getFoldersRoot() {
         return foldersRoot;
     }
 
+    private List<Folder> folderList = new ArrayList<Folder>();
+    public List<Folder> getFolderList() {
+        return this.folderList;
+    }
+
+    public EmailManager() {
+        folderUpdaterService = new FolderUpdaterService(folderList);
+        folderUpdaterService.start();
+    }
+
     public void addEmailAccount (EmailAccount emailAccount) {
         EmailTreeItem<String> treeItem = new EmailTreeItem<String>(emailAccount.getAddress());
-        FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem);
+        FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem, folderList);
         fetchFoldersService.start();
         foldersRoot.getChildren().add(treeItem);
     }
