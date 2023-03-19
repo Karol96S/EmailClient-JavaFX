498de64f4b6c11150e3e09ce29637f3f9c09f48b
Karol_S <szypa288@gmail.com>
2023-03-19 01:20:24 +0100

    added icons to treeview

--- src/main/java/com/client_email/EmailManager.java
+++ src/main/java/com/client_email/EmailManager.java
@@ -5,6 +5,7 @@ import com.client_email.controller.services.FolderUpdaterService;
 import com.client_email.model.EmailAccount;
 import com.client_email.model.EmailMessage;
 import com.client_email.model.EmailTreeItem;
+import com.client_email.view.IconResolver;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TreeItem;
@@ -20,6 +21,7 @@ public class EmailManager {
     private EmailMessage selectedMessage;
     private EmailTreeItem<String> selectedFolder;
     private ObservableList<EmailAccount> emailAccounts = FXCollections.observableArrayList();
+    private IconResolver iconResolver = new IconResolver();
 
     public ObservableList<EmailAccount> getEmailAccounts() {
         return emailAccounts;
@@ -61,6 +63,7 @@ public class EmailManager {
     public void addEmailAccount (EmailAccount emailAccount) {
         emailAccounts.add(emailAccount);
         EmailTreeItem<String> treeItem = new EmailTreeItem<String>(emailAccount.getAddress());
+        treeItem.setGraphic(iconResolver.getIconForFolder(emailAccount.getAddress()));
         FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem, folderList);
         fetchFoldersService.start();
         foldersRoot.getChildren().add(treeItem);
