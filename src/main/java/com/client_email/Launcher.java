6896896e525e2756cfa3acd16884c7882cdfadbb
Karol_S <szypa288@gmail.com>
2023-03-22 03:43:24 +0100

    persistence implemented

--- src/main/java/com/client_email/Launcher.java
+++ src/main/java/com/client_email/Launcher.java
@@ -1,5 +1,9 @@
 package com.client_email;
 
+import com.client_email.controller.persistence.PersistenceAccess;
+import com.client_email.controller.persistence.ValidAccount;
+import com.client_email.controller.services.LoginService;
+import com.client_email.model.EmailAccount;
 import com.client_email.view.ViewFactory;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,18 +11,42 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Launcher extends Application {
 
     public static void main(String[] args) {
         launch(args);
     }
 
+    private PersistenceAccess persistenceAccess = new PersistenceAccess();
+    private EmailManager emailManager = new EmailManager();
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        ViewFactory viewFactory = new ViewFactory(new EmailManager());
-        viewFactory.showLoginWindow();
-        viewFactory.updateStyles();
+        ViewFactory viewFactory = new ViewFactory(emailManager);
+        List<ValidAccount> validAccountList = persistenceAccess.loadFromPersistence();
+        if (validAccountList.size() > 0) {
+            viewFactory.showMainWindow();
+            for (ValidAccount validAccount: validAccountList) {
+                EmailAccount emailAccount = new EmailAccount(validAccount.getAddress(), validAccount.getPassword());
+                LoginService loginService = new LoginService(emailAccount, emailManager);
+                loginService.start();
+            }
+        } else {
+            viewFactory.showLoginWindow();
+        }
 
+    }
+
+    @Override
+    public void stop() throws Exception {
+        List<ValidAccount> validAccountList = new ArrayList<ValidAccount>();
+        for (EmailAccount emailAccount: emailManager.getEmailAccounts()) {
+            validAccountList.add(new ValidAccount(emailAccount.getAddress(), emailAccount.getPassword()));
+        }
+        persistenceAccess.saveToPersistence(validAccountList);
     }
 }
